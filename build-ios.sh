b81d21aef713f7d8f232704e06e560cc7c18bfa8
Simon Grätzer <simon@graetzer.org>
2012-10-02 23:52:30 +0200

    Added mpeg4 encoding

--- build-ios.sh
+++ build-ios.sh
@@ -14,6 +14,8 @@ echo Configure for armv7 build
 --enable-cross-compile \
 --enable-nonfree \
 --enable-gpl \
+--enable-encoder=mpeg1video \
+--enable-encoder=mpeg4 \
 --disable-armv5te \
 --disable-swscale-alpha \
 --disable-doc \
@@ -24,8 +26,10 @@ echo Configure for armv7 build
 --disable-asm \
 --disable-debug
 
+
 make clean
 make && make install
+
 
 echo Configure for i386
 ./configure \
@@ -41,6 +45,8 @@ echo Configure for i386
 --enable-cross-compile \
 --enable-nonfree \
 --enable-gpl \
+--enable-encoder=mpeg1video \
+--enable-encoder=mpeg4 \
 --disable-armv5te \
 --disable-swscale-alpha \
 --disable-doc \
@@ -58,34 +64,26 @@ make && make install
 # make fat (universal) libs
 mkdir -p ./compiled/fat/lib
 
-lipo -output ./compiled/fat/lib/libavcodec.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libavcodec.a  -create \
 -arch armv7 ./compiled/armv7/lib/libavcodec.a \
 -arch i386 ./compiled/i386/lib/libavcodec.a
 
-lipo -output ./compiled/fat/lib/libavdevice.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libavdevice.a  -create \
 -arch armv7 ./compiled/armv7/lib/libavdevice.a \
 -arch i386 ./compiled/i386/lib/libavdevice.a
 
-lipo -output ./compiled/fat/lib/libavformat.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libavformat.a  -create \
 -arch armv7 ./compiled/armv7/lib/libavformat.a \
 -arch i386 ./compiled/i386/lib/libavformat.a
 
-lipo -output ./compiled/fat/lib/libavutil.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libavutil.a  -create \
 -arch armv7 ./compiled/armv7/lib/libavutil.a \
 -arch i386 ./compiled/i386/lib/libavutil.a
 
-lipo -output ./compiled/fat/lib/libswresample.a  -create \
--arch armv7 ./compiled/armv7/lib/libswresample.a \
--arch i386 ./compiled/i386/lib/libswresample.a
-
-lipo -output ./compiled/fat/lib/libpostproc.a  -create \
--arch armv7 ./compiled/armv7/lib/libpostproc.a \
--arch i386 ./compiled/i386/lib/libpostproc.a
-
-lipo -output ./compiled/fat/lib/libswscale.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libswscale.a  -create \
 -arch armv7 ./compiled/armv7/lib/libswscale.a \
 -arch i386 ./compiled/i386/lib/libswscale.a
 
-lipo -output ./compiled/fat/lib/libavfilter.a  -create \
+xcrun -sdk iphoneos lipo -output ./compiled/fat/lib/libavfilter.a  -create \
 -arch armv7 ./compiled/armv7/lib/libavfilter.a \
 -arch i386 ./compiled/i386/lib/libavfilter.a
